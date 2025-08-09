5b723fcad430f48dc4a09fbfb757c01c3882b68f
rhn.id <drehan566@gmail.com>
2025-08-09 20:45:51 +0700

    new

--- dokumentasi/index.php
+++ dokumentasi/index.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Generasi Pencinta Alam</title>
-    <link rel=”icon” href=”../logo.jpg”>
+    <link rel="icon" href="../logo.jpg">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
